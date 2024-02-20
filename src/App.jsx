a9a8ab46b343e3c11d40c45af9203a3c67eb91c7
Rohit Verma <rohitverma0150@gmail.com>
2024-02-20 06:14:06 +0000

    PropDrilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import ChildA from './Components/PropDrilling/ChildA';
 import UseRef from './Components/UseRef';
 
 function App() {
+
+  const name = "Rohit"
   return (
     <div className="App">
       <UseRef/>
+      <ChildA name={name}/>
     </div>
   );
 }
